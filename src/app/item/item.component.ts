087f886865ae40426447995f14b6412c79e77615
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-11-23 09:46:12 +0200

    cleanup

--- src/app/item/item.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-	selector: 'app-item',
-	template: `<p>{{item}}</p>`,
-})
-export class ItemComponent {
-	@Input() item: string;
-}
